88dcbab2c006bebdde6505d7255a890bac44e959
loopy-lim <loopy@jnu.ac.kr>
2024-06-07 23:35:44 +0900

    feat: add saerhParams

--- components/questions/block.tsx
+++ components/questions/block.tsx
@@ -1,38 +1,62 @@
 import { Question } from "@/constants/questions";
 import { ElementType, ExtractAnswer } from "@/lib/typeHelper";
-import { useId } from "react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 interface QuestionBlockProps {
   data: Question;
+  questionIndex: number;
 }
 
 interface AnswerBlockProps {
   answer: ElementType<ExtractAnswer<Pick<Question, "answer">>>;
+  answerIndex: number;
+  questionIndex: number;
 }
 
-const AnswerBlock = ({ answer: { a } }: AnswerBlockProps) => {
-  const id = useId();
+const AnswerBlock = ({
+  answer: { a },
+  answerIndex,
+  questionIndex,
+}: AnswerBlockProps) => {
+  const searchParams = useSearchParams();
+  const currentAnswer = searchParams.get(`${questionIndex}`);
+  const params = Object.fromEntries(searchParams.entries());
 
   return (
     <div className="flex items-center">
-      <input className="hidden peer" type="radio" name="answer" id={id} />
-      <label
-        className="peer-checked:bg-slate-200 w-full rounded border border-slate-200 p-2 cursor-pointer"
-        htmlFor={id}
+      <Link
+        href={{
+          query: {
+            ...params,
+            [questionIndex]: answerIndex,
+            selected: questionIndex + 1,
+          },
+        }}
+        className={cn(
+          "w-full rounded border border-slate-200 p-2",
+          currentAnswer === `${answerIndex}` && "bg-slate-200"
+        )}
       >
         {a}
-      </label>
+      </Link>
     </div>
   );
 };
 
-export const QuestionBlock = ({ data }: QuestionBlockProps) => {
+export const QuestionBlock = ({ data, questionIndex }: QuestionBlockProps) => {
   return (
     <div className="break-keep">
       <h3 className="font-semibold text-xl my-4">{data.question}</h3>
       <div className="flex flex-col gap-2">
-        {data.answer.map((answer) => (
-          <AnswerBlock key={answer.a} answer={answer} />
+        {data.answer.map((answer, index) => (
+          <AnswerBlock
+            key={answer.a}
+            answer={answer}
+            answerIndex={index}
+            questionIndex={questionIndex}
+          />
         ))}
       </div>
     </div>
--- components/questions/index.tsx
+++ components/questions/index.tsx
@@ -11,21 +11,25 @@ import {
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export const QuestionBlocks = () => {
   const [api, setApi] = useState<CarouselApi>();
   const [isLast, setIsLast] = useState(false);
   const [isFirst, setIsFirst] = useState(true);
+  const searchParams = useSearchParams();
+  const params = Object.fromEntries(searchParams.entries());
 
   useEffect(() => {
     if (!api) {
       return;
     }
-    api.on("select", () => {
-      setIsFirst(api.selectedScrollSnap() === 0);
-      setIsLast(api.selectedScrollSnap() === QUESTIONS.length - 1);
-    });
-  }, [api]);
+    const selected = parseInt(params["selected"] as string, 10) || 0;
+    api.scrollTo(selected);
+    setIsFirst(selected === 0 || selected === undefined);
+    setIsLast(selected === QUESTIONS.length - 1);
+  }, [api, params]);
 
   return (
     <Carousel
@@ -35,25 +39,49 @@ export const QuestionBlocks = () => {
       <CarouselContent>
         {QUESTIONS.map((question, index) => (
           <CarouselItem key={index}>
-            <QuestionBlock data={question} />
+            <QuestionBlock data={question} questionIndex={index} />
           </CarouselItem>
         ))}
       </CarouselContent>
       <div className="flex gap-4 w-full">
-        <Button
-          className={cn("w-full", isFirst && "bg-gray-600")}
-          disabled={isFirst}
-          onClick={() => api?.scrollPrev()}
+        <Link
+          className="w-full"
+          href={{
+            query: {
+              ...params,
+              selected: Math.max(
+                (parseInt(params["selected"] as string, 10) || 0) - 1,
+                0
+              ),
+            },
+          }}
         >
-          뒤로
-        </Button>
-        <Button
-          className={cn("w-full", isLast && "bg-gray-600")}
-          onClick={() => api?.scrollNext()}
-          disabled={isLast}
+          <Button
+            className={cn("w-full", isFirst && "bg-gray-600")}
+            disabled={isFirst}
+          >
+            뒤로
+          </Button>
+        </Link>
+        <Link
+          className="w-full"
+          href={{
+            query: {
+              ...params,
+              selected: Math.min(
+                (parseInt(params["selected"] as string, 10) || 0) + 1,
+                QUESTIONS.length - 1
+              ),
+            },
+          }}
         >
-          앞으로
-        </Button>
+          <Button
+            className={cn("w-full", isLast && "bg-gray-600")}
+            disabled={isLast}
+          >
+            앞으로
+          </Button>
+        </Link>
       </div>
     </Carousel>
   );
