51c13851a68165f00fd38a932fc7589ec9c29235
loopy-lim <loopy@jnu.ac.kr>
2024-06-07 14:25:31 +0900

    feat: add carousel

--- components/questions/index.tsx
+++ components/questions/index.tsx
@@ -1,8 +1,21 @@
 import { QUESTIONS } from "@/constants/questions";
 import { QuestionBlock } from "./block";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 
 export const QuestionBlocks = () => {
-  return QUESTIONS.map((question, index) => (
-    <QuestionBlock key={index} data={question} />
-  ));
+  return (
+    <Carousel
+      opts={{
+        active: false,
+      }}
+    >
+      <CarouselContent>
+        {QUESTIONS.map((question, index) => (
+          <CarouselItem key={index}>
+            <QuestionBlock data={question} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
 };
