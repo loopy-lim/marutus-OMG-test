4c3cfe6c8b9aa107e1bcb347978af95646cd22eb
loopy-lim <loopy@jnu.ac.kr>
2024-06-08 00:17:12 +0900

    refactor: file seperate

---
+++ components/questions/Move.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { QUESTIONS } from "@/constants/questions";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { CarouselApi } from "../ui/carousel";
+import { cn } from "@/lib/utils";
+
+interface QusetionMoveButtonsProps {
+  api?: CarouselApi;
+}
+
+export const QusetionMoveButtons = ({ api }: QusetionMoveButtonsProps) => {
+  const [isLast, setIsLast] = useState(false);
+  const [isFirst, setIsFirst] = useState(true);
+  const searchParams = useSearchParams();
+  const params = Object.fromEntries(searchParams.entries());
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    const selected = parseInt(params["selected"] as string, 10) || 0;
+    api.scrollTo(selected);
+    setIsFirst(selected === 0 || selected === undefined);
+    setIsLast(selected === QUESTIONS.length - 1);
+  }, [api, params]);
+
+  return (
+    <div className="flex gap-4 w-full">
+      <Link
+        className="w-full"
+        href={{
+          query: {
+            ...params,
+            selected: Math.max(
+              (parseInt(params["selected"] as string, 10) || 0) - 1,
+              0
+            ),
+          },
+        }}
+      >
+        <Button
+          className={cn("w-full", isFirst && "bg-gray-600")}
+          disabled={isFirst}
+        >
+          뒤로
+        </Button>
+      </Link>
+      <Link
+        className="w-full"
+        href={{
+          query: {
+            ...params,
+            selected: Math.min(
+              (parseInt(params["selected"] as string, 10) || 0) + 1,
+              QUESTIONS.length - 1
+            ),
+          },
+        }}
+      >
+        <Button
+          className={cn("w-full", isLast && "bg-blue-800 hover:bg-blue-700")}
+        >
+          {isLast ? "결과보기" : "앞으로"}
+        </Button>
+      </Link>
+    </div>
+  );
+};
--- components/questions/index.tsx
+++ components/questions/index.tsx
@@ -9,15 +9,11 @@ import {
   CarouselItem,
 } from "../ui/carousel";
 import { useEffect, useState } from "react";
-import { Button } from "../ui/button";
-import { cn } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+import { QusetionMoveButtons } from "./Move";
 
 export const QuestionBlocks = () => {
   const [api, setApi] = useState<CarouselApi>();
-  const [isLast, setIsLast] = useState(false);
-  const [isFirst, setIsFirst] = useState(true);
   const searchParams = useSearchParams();
   const params = Object.fromEntries(searchParams.entries());
 
@@ -27,12 +23,13 @@ export const QuestionBlocks = () => {
     }
     const selected = parseInt(params["selected"] as string, 10) || 0;
     api.scrollTo(selected);
-    setIsFirst(selected === 0 || selected === undefined);
-    setIsLast(selected === QUESTIONS.length - 1);
   }, [api, params]);
 
   return (
     <Carousel
+      opts={{
+        watchDrag: false,
+      }}
       setApi={setApi}
       className="flex flex-col min-h-screen justify-center gap-4 px-4"
     >
@@ -43,46 +40,7 @@ export const QuestionBlocks = () => {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <div className="flex gap-4 w-full">
-        <Link
-          className="w-full"
-          href={{
-            query: {
-              ...params,
-              selected: Math.max(
-                (parseInt(params["selected"] as string, 10) || 0) - 1,
-                0
-              ),
-            },
-          }}
-        >
-          <Button
-            className={cn("w-full", isFirst && "bg-gray-600")}
-            disabled={isFirst}
-          >
-            뒤로
-          </Button>
-        </Link>
-        <Link
-          className="w-full"
-          href={{
-            query: {
-              ...params,
-              selected: Math.min(
-                (parseInt(params["selected"] as string, 10) || 0) + 1,
-                QUESTIONS.length - 1
-              ),
-            },
-          }}
-        >
-          <Button
-            className={cn("w-full", isLast && "bg-gray-600")}
-            disabled={isLast}
-          >
-            앞으로
-          </Button>
-        </Link>
-      </div>
+      <QusetionMoveButtons api={api} />
     </Carousel>
   );
 };
