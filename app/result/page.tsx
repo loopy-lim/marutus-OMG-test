374ca6a087a6fe6d108fb0c90bd585f9617d4d57
loopy-lim <loopy@jnu.ac.kr>
2024-06-08 21:25:20 +0900

    feat: add result barrier
    - when all question not clear, redirect play

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,6 +1,25 @@
 import { Result } from "@/components/result";
+import { QUESTIONS } from "@/constants/questions";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+interface ResultProps {
+  searchParams: { [key: string]: string };
+}
+
+export default function Home({ searchParams }: ResultProps) {
+  const paramString = Object.keys(searchParams)
+    .map((key) => `${key}=${searchParams[key]}`)
+    .join("&");
+
+  if (
+    !Array.from(
+      { length: QUESTIONS.length },
+      (_, i) => searchParams[i.toString()]
+    ).every((v) => v != null)
+  ) {
+    redirect(`/play?${paramString}`);
+  }
+
   return (
     <div className="absolute min-h-screen w-[420px] right-24 bg-white">
       <Result />
